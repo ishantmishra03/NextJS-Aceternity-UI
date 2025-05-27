65e56b50fa934cddf79cde3dee6b225cb067164d
Dev <=>
2025-05-27 13:41:03 +0545

    NextJS frontedn with aceternity UI improved

--- src/components/ui/sticky-scroll-reveal.tsx
+++ src/components/ui/sticky-scroll-reveal.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
-import { useMotionValueEvent, useScroll } from "motion/react";
+import { useMotionValueEvent, useScroll } from "framer-motion";
 import { motion } from "motion/react";
 import { cn } from "@/lib/utils";
 
